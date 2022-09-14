12f49b78f36c0800c1b44c691bba06b123a513fc
zhuyuan7 <jwjwchoi0910@gmail.com>
2022-09-14 19:19:41 +0900

    data_address change

--- code/arg.py
+++ code/arg.py
@@ -3,7 +3,7 @@ import argparse
 def arg_parser():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--predict_txt', default="/home/joo/name_data/data/test_label_2.csv",type=str)
-    parser.add_argument('--raw_txt', default="/home/joo/name_data/data/names/all_data.csv",type=str)                    
+    parser.add_argument('--raw_txt', default="/home/joo/name_data/data/all_data.csv",type=str)                    
     parser.add_argument('--lr', default=2e-5,type=float)
     parser.add_argument('--eps', default=1e-5,type=float)
     parser.add_argument('--epochs', default=5,type=int) 
